4e91027f344523ee44fdfed9d70298849441ab97
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-11-12 09:15:54 -0800

    printf char function added

--- main.h
+++ main.h
@@ -12,8 +12,12 @@ int _printf(const char *format, ...);
 #include <stdlib.h>
 #include <limits.h>
 
+//_printf.c
 int _printf(const char *format, ...);
+//_putchar.c
 int _putchar(char c);
+//printf_char
+int printf_char(va_list val);
 >>>>>>> 3185a3317b39e1637b24705ce979b7f21a2da74c
 
 #endif
---
+++ test.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+int main(void){
+int print_char('c')
+}
