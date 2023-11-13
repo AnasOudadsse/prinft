dd8711b25b0d5fd22b975762bc0e6f396de3d73b
Abdelnissoul <abdelnissoul205@gmail.com>
2023-11-13 10:16:03 +0000

    Correcting some issues

--- main.h
+++ main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-<<<<<<< HEAD
+
 int _printf(const char *format, ...);
 
-=======
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
@@ -12,12 +11,11 @@ int _printf(const char *format, ...);
 #include <stdlib.h>
 #include <limits.h>
 
-//_printf.c
+/*_printf.c */
 int _printf(const char *format, ...);
-//_putchar.c
+/*_putchar.c */
 int _putchar(char c);
-//printf_char
+/* printf_char */
 int printf_char(va_list val);
->>>>>>> 3185a3317b39e1637b24705ce979b7f21a2da74c
 
 #endif
--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
-int main(void){
-int print_char('c')
+int main(void)
+{
+int print_char('c');
 }
