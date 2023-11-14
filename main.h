70d638f9773af54b880734019dd85e40738350d7
Abdelnissoul <abdelnissoul205@gmail.com>
2023-11-14 10:31:34 +0000

    correcting some issues

--- main.h
+++ main.h
@@ -10,25 +10,25 @@ int _printf(const char *format, ...);
 #include <stdlib.h>
 #include <limits.h>
 
-//_printf.c
+/*_printf.c*/
 int _printf(const char *format, ...);
 /*_putchar.c */
 int _putchar(char c);
 /* print_c.c */
 int printf_char(va_list val);
-//print_s.c
+/*print_s.c*/
 int printf_string(va_list agr);
-//_strlen.c
+/*_strlen.c*/
 int _strlen(char *str);
 int _strlenc(const char *str);
-//print_%.c
-int printf_37(void):
-//print_int.c
+/*print_%.c */
+int printf_37(void);
+/*print_int.c*/
 int print_i(va_list args);
 int print_d(va_list args);
-//print_b.c
-int print_b(va_list val)
-//print_Hex.c
-int print_HEX(va_list val)
+/*print_b.c*/
+int print_b(va_list val);
+/*print_Hex.c*/
+int print_HEX(va_list val);
 
 #endif
