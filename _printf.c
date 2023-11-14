20409b61ae00eb3bee2663e8fdf9da5827b66f14
AnasOudadsse <anas.oudadsse1@gmail.com>
2023-11-14 14:07:14 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -12,43 +12,29 @@ int _printf(const char *format, ...)
 	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	c = 0;
-	while (format[c])
+	for (c = 0; format[c]; c++)
 	{
 		if (format[c] == '%')
 		{
-			switch (format[c + 1])
-			{
-				case '%':
-					_putchar('%');
-					c = c + 1;
-					length++;
-				break;
-				case 'c':
-				case 's':
-				case 'i':
-				case 'd':
-				case 'b':
-					length += handle_match(format, c + 1)(arguments);
-					c = c + 1;
-				break;
-				default:
-					_putchar(format[c]);
-					length++;
-					break;
+			if (format[c + 1] == '%')
+			{   _putchar('%');
+				c = c + 1;
+				length++;
+			}
+			else if (handle_match(format, c + 1) != NULL)
+			{   length += handle_match(format, c + 1)(arguments);
+				c = c + 1;
+			}
+			else
+			{ _putchar(format[c]);
+				length++;
 			}
 		}
 		else
-		{
-			_putchar(format[c]);
+		{ _putchar(format[c]);
 			length++;
 		}
-		c++;
 	}
 	va_end(arguments);
 	return (length);
-}
-int main(void)
-{
-return (0);
 }
